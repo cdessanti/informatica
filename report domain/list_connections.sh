05d146ee76e101d548f938d06e48f83e4d5068ad
Candido Dessanti <30822551+cdessanti@users.noreply.github.com>
2024-03-11 16:26:29 +0100

    Update list_connections.sh
    add filter

--- report domain/list_connections.sh
+++ report domain/list_connections.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 function show_usage {
-  echo "usage: list_connections.sh domain_name repository_name admin_user password"
+  echo "usage: list_connections.sh domain_name repository_name admin_user password [filter]"
 }
-if [ "$?" != "4" ]; then
-  show_usage
-fi;
+# if [ "$*" != "4" ]; then
+#   show_usage
+# fi;
 pmrep connect -r $2 -d $1 -n $3 -x $4 2>&1 >/dev/null
 if [ "$?" != "0" ]; then
   echo "cannot connect exiting";
   show_usage
 fi;
-connections=$(pmrep listconnections -t | egrep "relational|loader|ftp")
+if [ "$5" != "" ]; then
+  filter=$5
+else
+  filter='.*'
+fi;
+
+connections=$(pmrep listconnections -t | egrep "relational|loader|ftp" | egrep $filter )
 details_file=connection_details_for_repository_$2.txt
 if test -f "$details_file"; then
  rm $details_file 2>&1 >/dev/null
